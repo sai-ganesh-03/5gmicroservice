8455be5ffbe2b089f46962dbe7636b01709e5528
SAI GANESH S <83502872+dude-curious@users.noreply.github.com>
2023-09-21 15:43:44 +0530

    first commit

---
+++ nrf/nrf.js
@@ -0,0 +1,21 @@
+// nrf.js (NRF microservice)
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// Simulated service registration information (replace with actual data)
+const serviceRegistry = {
+  registration: "udm-service:3000", // Map the 'registration' service to the UDM microservice
+  // Add more service mappings as needed
+};
+
+// Serve the serviceRegistry JSON
+app.get("/service-registry", (req, res) => {
+  res.json(serviceRegistry);
+});
+
+// Define routes and functionality for other services
+
+app.listen(port, () => {
+  console.log(`NRF microservice is running on port ${port}`);
+});
